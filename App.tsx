b82b193e356146c86f59c2a42e6d705d488c4be3
Pongpaii <onekickman11@gmail.com>
2024-08-03 11:30:43 +0700

    pracEffect2

--- App.tsx
+++ App.tsx
@@ -17,31 +17,38 @@ export default function App(): React.JSX.Element {
   //sete state func
   const [fullname, setFullname] = useState("");
   const [message, setMessage] = useState("Message from App.tsx");
-  const [footerMessage, setFootermessage] = useState("Thai Nichi Institude of Technology");
+  const [footerMessage, setFootermessage] = useState(
+    "Thai Nichi Institude of Technology"
+  );
 
   //use effect
-  useEffect(()=>{
-    console.log('component has mounted')
-  },[]);
-  useEffect(()=>{
-    console.log(`FullName has Changed to : ${fullname}` )
-  },[fullname]); //show log when full name change
+  useEffect(() => {
+    console.log("component has mounted");
+  }, []);
+  useEffect(() => {
+    console.log(`FullName has Changed to : ${fullname}`);
+  }, [fullname]); //show log when full name change
+
+  //create function to send this function to the contentinthe line44
+  const handleButtonClick=()=>{
+    Alert.alert("Hello", `Input your fullname : ${fullname}`);
+  };
 
 
 
   //uxui
   return (
     <View style={stylesPractice.container}>
-      <Appheader fullname={fullname} message={message}/>
-      <Content  message={ message} fullname={fullname}/>
+      <Appheader fullname={fullname} message={message} />
+      {/* <Content message={message} fullname={fullname} /> */}
+      <Content message={message} onButtonClick={handleButtonClick} />
       <TextInput
-      style = {stylesPractice.input}
-      placeholder="Enter your full name"
-      value={fullname}
-      onChangeText={setFullname}
+        style={stylesPractice.input}
+        placeholder="Enter your full name"
+        value={fullname}
+        onChangeText={setFullname}
       />
-      <Appfooter footerMessage={footerMessage}/>
-    
+      <Appfooter footerMessage={footerMessage} />
     </View>
   );
 }
--- components/Content.tsx
+++ components/Content.tsx
@@ -4,23 +4,24 @@ import { stylesPractice } from "../styles/styles";
 
 interface ContentProps {
   message: string;
-  fullname: string;
+  //fullname: string;
+  onButtonClick:()=>void;//now content get this function
 }
-const Content = ({ message, fullname }: ContentProps): React.JSX.Element => {
-  const [displayFullname, setDisplayFullname] = React.useState("");
+const Content = ({ message, onButtonClick }: ContentProps): React.JSX.Element => {
+/*   const [displayFullname, setDisplayFullname] = React.useState("");
   const handleButtonClick = () => {
     setDisplayFullname(fullname);
     Alert.alert("Hello", `Input your fullname : ${fullname}`);
   };
-
-  const onCLickMe = () => {
+ */
+/*   const onCLickMe = () => {
     Alert.alert("HELLO ", fullname);
-  };
+  }; */
   return (
     <View style={stylesPractice.content}>
       <Text style={stylesPractice.text}>{message}</Text>
-      <Text style={stylesPractice.text}>{displayFullname}</Text>
-      <Button title="click me" onPress={handleButtonClick} />
+      {/* <Text style={stylesPractice.text}>{displayFullname}</Text> */}
+      <Button title="click me" onPress={onButtonClick} />
     </View>
   );
 };
