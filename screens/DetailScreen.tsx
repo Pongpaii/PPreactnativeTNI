09a4c938e8b946a1b0fc9965211a7c62e8bebebf
Pongpaii <onekickman11@gmail.com>
2024-09-14 11:54:44 +0700

    Drawer Navi

--- screens/DetailScreen.tsx
+++ screens/DetailScreen.tsx
@@ -1,4 +1,4 @@
-import { View, Text,ActivityIndicator } from 'react-native'
+import { View, Text,ActivityIndicator,StyleSheet } from 'react-native'
 import React from 'react'
 import {useNavigation,useRoute} from '@react-navigation/native'
 import {findProductbyId} from '../services/product-service'
@@ -8,6 +8,8 @@ import { useState } from 'react'
 import { FlatList } from 'react-native-gesture-handler'
 import { ListItem,Avatar } from '@rneui/base'
 import { ListRenderItem } from 'react-native'
+import { Tile } from '@rneui/themed';
+import { Dimensions } from 'react-native'
 
 
 const DetailScreen = ():React.JSX.Element => {
@@ -42,16 +44,27 @@ const DetailScreen = ():React.JSX.Element => {
   const renderItem: ListRenderItem<any> = ({ item }) => {
     return (
       <>
-        <ListItem.Content>
-          <ListItem.Title>{item.ch_title}</ListItem.Title>
-        </ListItem.Content>
+    <Tile
+                imageSrc={{
+                    uri: 'https://pbs.twimg.com/amplify_video_thumb/1833798294000345089/img/SeKjyfJHHcWwboaZ.jpg',
+                    cache: 'force-cache',
+                }}
+                title={item.ch_title}
+                titleStyle={styles.titleStyle}
+                containerStyle={styles.tileContainer}
+                caption={item.ch_dateadd}
+                captionStyle={styles.captionStyle}
+                featured
+                activeOpacity={0.9}
+                width={Dimensions.get('screen').width-20} // ลบขอบเล็กน้อยทั้งสองข้าง
+            />
       </>
     );
   };
   return (
     <View>
        <View>
-       <FlatList
+       <FlatList contentContainerStyle={styles.listContent}
         data={detail}
         keyExtractor={(item: any) => item.ch_id.toString()}
         renderItem={renderItem}
@@ -62,3 +75,30 @@ const DetailScreen = ():React.JSX.Element => {
 }
 
 export default DetailScreen
+const styles = StyleSheet.create({
+  container: {
+  backgroundColor: '#f0f0f0', // สีพื้นหลัง
+  },
+  listContent: {
+  paddingHorizontal: 10, // การเว้นวรรคขอบซ้ายและขวาเท่ากัน
+  },
+  titleStyle: {
+  fontSize: 20,
+  fontWeight: 'bold',
+  color: 'brown', // สีขาวส าหรับชื่อ
+  },
+  tileContainer: {
+  borderRadius: 10, //ก าหนดให้มุมของ container มีความโค้งมน
+  overflow: 'hidden', //ก าหนดให้เนื้อหาที่อาจล้นออกจากขอบของ container ไม่แสดงผล
+  marginBottom: 10, //ก าหนดระยะห่างจากขอบล่างของ container ไปยัง element ต่อไปที่อยู่ด้านล่าง
+  elevation: 5, // เงาส าหรับ Android
+  shadowOffset: { width: 0, height: 2 }, //ก าหนดต าแหน่งของเงาที่แสดงผล
+  shadowOpacity: 0.25, //ก าหนดระดับความโปร่งแสงของเงา
+  shadowRadius: 3.84, //ก าหนดรัศมีของการกระจายตัวของเงา
+  opacity: 0.5, // ความโปร่งใสของภาพ , ค่าน้อยจะโปร่งใสมาก
+  },
+  captionStyle: {
+  fontSize: 14,
+  color: 'blue' // สีขาวส าหรับวันที่
+  },
+  });
